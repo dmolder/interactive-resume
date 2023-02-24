2670c53a942158c7375b9e6522ffe22498604bb4
Daniel Molder <danielm9865@att.net>
2023-02-23 20:54:43 -0600

    Started routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'about', component: AboutComponent},
+    { path: 'experience', component: ExperienceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
